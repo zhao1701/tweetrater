fbff2ab967699f2b3ae5d00d81d039c0e903ff09
Derek Zhao <dzhao@svg.openit.local>
2017-07-16 12:49:34 +0100

    working ajax prediction

--- webapp/test.py
+++ webapp/test.py
@@ -1,16 +1,16 @@
-from flask import Flask, redirect, url_for, request, render_template
+from flask import Flask, redirect, url_for, request, render_template, jsonify
 from helpers import tweet_rater
 
 app = Flask('__main__')
 
-@app.route('/')
+@app.route('/home/', methods=['GET','POST'])
 def index():
 	return render_template('test_index.html')
 
-@app.route('/predict/', methods = ['POST'])
+@app.route('/predict/', methods = ['GET','POST'])
 def predict():
-	tweet = request.form['tweet']
-	return tweet + '<br/>' + tweet_rater(tweet)
+	tweet = request.args.get('tweet')
+	return jsonify(prediction=tweet_rater(tweet))
 
 if __name__ == '__main__':
 	app.run(debug=True)
