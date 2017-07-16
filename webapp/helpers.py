c200e1589cf3c00bebacd884b5e771ac31fb8521
Derek Zhao <dzhao@svg.openit.local>
2017-07-16 13:47:28 +0100

    add sample retrieval helpers

--- webapp/helpers.py
+++ webapp/helpers.py
@@ -2,8 +2,9 @@ import re
 import spacy
 import pickle as pkl
 import numpy as np
-import tensorflow as tf
+import pandas as pd
 
+from tensorflow import get_default_graph
 from keras.models import load_model
 from keras.preprocessing.sequence import pad_sequences
 from keras.preprocessing.text import Tokenizer
@@ -16,9 +17,10 @@ with open('static/neural_preprocessing', 'rb') as file_in:
 	max_sequence_length = neural_preprocessing['max_sequence_length']
 
 cnn = load_model('static/model_cnn')
-graph = tf.get_default_graph()
+graph = get_default_graph()
 nlp = spacy.load('en')
 
+### TWEET RATER ###
 
 # tweet-cleaning functions
 replace_user = lambda tweet: re.sub(r'(@\w+\s*)', r'TWITTER_HANDLE ', tweet)
@@ -43,3 +45,23 @@ def tweet_rater(tweet):
         return 'I\'m {:2.4}% sure that\'s offensive.'.format(probas[1]*100)
     else:
         return 'I\'m {:2.4}% sure that\'s hate speech.'.format(probas[2]*100)
+
+### BEAT THE MODEL ###
+
+# load training and test data
+with open('static/data_all', 'rb') as file_in:
+    data_all = pkl.load(file_in)
+    df_orig = data_all['df_orig']
+    X_train = data_all['X_train']
+    y_test = data_all['y_test']
+
+    df_orig_train = df_orig.reindex(X_train.index)
+    df_orig_test = df_orig.reindex(y_test.index)
+
+def get_training_samples(n_samples=10, rating=0, data=df_orig_train):
+    return data[data.rating == rating].text.sample(n_samples).values
+
+def get_test_samples(n_samples=10, data=df_orig_train):
+    return data.text.sample(n_samples).values
+
+print(get_test_samples(5))
