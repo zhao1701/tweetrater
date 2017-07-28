f408ce6d636f6f3981d4af927018448c8f1de3d9
Derek Zhao <dzhao@svg.openit.local>
2017-07-28 08:31:48 +0100

    remove deployments

--- webapp/test_app/static/js/about_script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	$('.section').hide().show('drop', 2000);
-	$('.col-sm-6').hide().fadeIn(3000);
-	$('.navbar').click(function() {
-		$('col-sm-6').fadeOut(3000);
-	});
-});
